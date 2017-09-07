9ade41f2c054af7b46aac4958b8fab1c5d6baaed
gaurav <saist1993@gmail.com>
2017-09-08 00:31:24 +0200

    Added phrase similarity using word2vec and average cosine

---
+++ utils/phrase_similarity.py
@@ -0,0 +1,46 @@
+'''
+    calculates the simialrity between two phrases using word2vec
+
+'''
+
+import gensim
+import numpy as np
+import traceback
+
+model = gensim.models.KeyedVectors.load_word2vec_format('/home/gaurav/Downloads/models/word2vec/GoogleNews-vectors-negative300.bin', binary=True)
+
+def ConvertVectorSetToVecAverageBased(vectorSet, ignore = []):
+		if len(ignore) == 0:
+			return np.mean(vectorSet, axis = 0)
+		else:
+			return np.dot(np.transpose(vectorSet),ignore)/sum(ignore)
+
+
+def phrase_similarity(_phrase_1, _phrase_2):
+    phrase_1 = _phrase_1.split(" ")
+    phrase_2 = _phrase_2.split(" ")
+    vw_phrase_1 = []
+    vw_phrase_2 = []
+    for phrase in phrase_1:
+        try:
+            print phrase
+            vw_phrase_1.append(model.word_vec(phrase))
+            print type(model.vw(phrase))
+        except:
+            print traceback.print_exc()
+            continue
+    for phrase in phrase_2:
+        try:
+            vw_phrase_2.append(model.word_vec(phrase))
+        except:
+            continue
+    print vw_phrase_1
+    print vw_phrase_2
+    if len(vw_phrase_1) == 0 or len(vw_phrase_2) == 0:
+        return 0
+    v_phrase_1 = ConvertVectorSetToVecAverageBased(vw_phrase_1)
+    v_phrase_2 = ConvertVectorSetToVecAverageBased(vw_phrase_2)
+    cosine_similarity = np.dot(v_phrase_1, v_phrase_2) / (np.linalg.norm(v_phrase_1) * np.linalg.norm(v_phrase_2))
+    return cosine_similarity
+
+print phrase_similarity("military branchere","child organization")

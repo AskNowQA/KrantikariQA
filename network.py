6b81d63c9513a3f4864898eb7ca28bbfc3f4d86c
gaurav <saist1993@gmail.com>
2017-12-17 01:03:49 +0100

    Dirty commit for network.py

--- network.py
+++ network.py
@@ -1,12 +1,53 @@
-import keras
+import keras,math
+import cPickle as pickle
+import numpy as np
 from keras.layers import Input, LSTM, Dense
 from keras.models import Model
 
+from keras import backend as K
+import os
 
+def set_keras_backend(backend):
+
+    if K.backend() != backend:
+        os.environ['KERAS_BACKEND'] = backend
+        reload(K)
+        assert K.backend() == backend
+
+set_keras_backend("tensorflow")
+
+
+#data loading from the secondary memory
+FILE_PATH = 'data/training/100'
+paths = pickle.load(open(FILE_PATH + '/P'))
+questions = pickle.load(open(FILE_PATH + '/Q'))
+true_paths = pickle.load(open(FILE_PATH + '/Y'))
+#
+x_path_train = np.asarray(paths[:int(len(paths)*.80)]).astype('float32')
+y_train = np.asarray(true_paths[:int(len(true_paths)*.80)]).astype('float32')
+x_path_test = np.asarray(paths[int(len(paths)*.80):]).astype('float32')
+y_test = np.asarray(true_paths[int(len(true_paths)*.80):]).astype('float32')
+q_path_train = np.asarray(questions[:int(len(questions)*.80)]).astype('float32')
+q_path_test = np.asarray(questions[int(len(questions)*.80):]).astype('float32')
+print(x_path_train.shape[0], 'train samples')
+
+# x_path_train = np.random.rand((800,24,300))
+# q_path_train = np.random.rand((800,24,300))
+# x_path_test = np.random.rand((200,24,300))
+# q_path_test = np.random.rand((200,24,300))
+# y_train = np.random.rand((800,1))
+# y_test = np.random.rand((200,1))
+
+path_input_shape = x_path_train.shape[1:]
+question_input_shape = q_path_train.shape[1:]
+
+# path_input_shape = x_path_train.shape
+# question_input_shape = q_path_train.shape
 # Declaring input tensors
-x_ques = Input(shape=(20, 300))
-x_path = Input(shape=(20, 300))
+x_ques = Input(shape=question_input_shape)
+x_path = Input(shape=path_input_shape)
 # neg_path = Input(shape=(20, 300))
+
 
 '''
     Encoder
@@ -36,7 +77,9 @@ classifier = Dense(1, activation='sigmoid')
 y = classifier(merged_path_ques)
 # y_neg = classifier(merged_negpath_ques)
 
-model = Model(inputs=[x_ques, x_path], outputs=y)
+model = Model(inputs=[ques_encoder, path_encoder], outputs=y)
+
 model.compile(optimizer=["sgd"],
               loss="binary_crossentropy",
               metrics=["precision", "recall", "accuracy"])
+model.fit([x_path_train,q_path_train],y_train,batch_size=1,epochs=100)

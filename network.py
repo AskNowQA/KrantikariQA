4e7122ceee29724f936890745653fa0c57231fe1
Priyansh Trivedi <pc.priyansh@gmail.com>
2017-12-18 12:47:20 +0100

    Sanity Commit

--- network.py
+++ network.py
@@ -9,13 +9,11 @@ from keras.layers.merge import concatenate
 import cPickle as pickle
 import numpy as np
 
-
-
-#data loading from the secondary memory
-FILE_PATH = 'data/training/100'
-paths = pickle.load(open(FILE_PATH + '/P'))
-questions = pickle.load(open(FILE_PATH + '/Q'))
-true_paths = pickle.load(open(FILE_PATH + '/Y'))
+# Pull the data up from disk
+FILE_PATH = 'data/training/small_runs'
+paths = np.load(open(FILE_PATH + '/P.npz'))
+questions = np.load(open(FILE_PATH + '/Q.npz'))
+true_paths = np.load(open(FILE_PATH + '/Y.npz'))
 
 
 x_path_train = np.asarray(paths[:int(len(paths)*.80)]).astype('float32')
@@ -30,7 +28,6 @@ print(x_path_train.shape[0], 'train samples')
 path_input_shape = x_path_train.shape[1:]
 question_input_shape = q_path_train.shape[1:]
 
-
 # Encode the pos and neg path using the same path encoder and also the question
 x_ques = Input(shape=path_input_shape)
 ques_encoder = LSTM(64)(x_ques)
@@ -41,10 +38,12 @@ path_encoder = LSTM(64)(x_path)
 merge = concatenate([ques_encoder, path_encoder])
 
 output = Dense(1, activation='sigmoid')(merge)
+
+# Model time!
 model = Model(inputs=[x_ques,x_path], outputs=output)
 print(model.summary())
 model.compile(optimizer='rmsprop',
               loss='binary_crossentropy',
               metrics=['accuracy'])
 
-model.fit([x_path_train,q_path_train],y_train,batch_size=1,epochs=100)
+model.fit([x_path_train, q_path_train], y_train, batch_size=1, epochs=100)

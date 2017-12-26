265f5962ef4c1ae4f080a31a91dbf36100ebdd17
Nilesh Chakraborty <nilesh@nileshc.com>
2017-12-26 22:17:06 +0100

    Add glove phrase similarity

--- utils/phrase_similarity.py
+++ utils/phrase_similarity.py
@@ -7,8 +7,15 @@
 import gensim
 import numpy as np
 # /home/gaurav/Downloads/models/word2vec
-model = gensim.models.KeyedVectors.load_word2vec_format('resources/GoogleNews-vectors-negative300.bin', binary=True)
+word2vec_embeddings = gensim.models.KeyedVectors.load_word2vec_format('resources/GoogleNews-vectors-negative300.bin', binary=True)
 # model = gensim.models.KeyedVectors.load_word2vec_format('/home/gaurav/Downloads/models/word2vec/GoogleNews-vectors-negative300.bin', binary=True)
+glove_embeddings = {}
+with open('resources/glove.42B.300d.txt') as f:
+    for line in f:
+        values = line.split()
+        word = values[0]
+        coefs = np.asarray(values[1:], dtype='float32')
+        glove_embeddings[word] = coefs
 
 def ConvertVectorSetToVecAverageBased(vectorSet, ignore = []):
 		if len(ignore) == 0:
@@ -17,7 +24,7 @@ def ConvertVectorSetToVecAverageBased(vectorSet, ignore = []):
 			return np.dot(np.transpose(vectorSet),ignore)/sum(ignore)
 
 
-def phrase_similarity(_phrase_1, _phrase_2):
+def phrase_similarity(_phrase_1, _phrase_2, embedding = 'word2vec'):
     phrase_1 = _phrase_1.split(" ")
     phrase_2 = _phrase_2.split(" ")
     vw_phrase_1 = []
@@ -25,13 +32,15 @@ def phrase_similarity(_phrase_1, _phrase_2):
     for phrase in phrase_1:
         try:
             # print phrase
-            vw_phrase_1.append(model.word_vec(phrase.lower()))
+            vw_phrase_2.append(word2vec_embeddings.word_vec(phrase.lower() if embedding == 'word2vec'
+                else glove_embeddings[phrase.lower()])
         except:
             # print traceback.print_exc()
             continue
     for phrase in phrase_2:
         try:
-            vw_phrase_2.append(model.word_vec(phrase.lower()))
+            vw_phrase_2.append(word2vec_embeddings.word_vec(phrase.lower() if embedding == 'word2vec'
+                else glove_embeddings[phrase.lower()])
         except:
             continue
     if len(vw_phrase_1) == 0 or len(vw_phrase_2) == 0:

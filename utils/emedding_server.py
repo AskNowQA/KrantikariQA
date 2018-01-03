799654a4954b9643f54feb29c2f8ca1e0ad23377
Priyansh Trivedi <pc.priyansh@gmail.com>
2018-01-03 14:47:46 +0100

    Intermediate Commit - Server file well underway. - serialize numpy mats - phase out original interface

--- utils/emedding_server.py
+++ utils/emedding_server.py
@@ -1,7 +1,7 @@
 """
-    Script does all the magic with word embeddings including lazy loading them in the RAM and all that.
+    Script that runs an embedding server and is expected to be left alone in the RAM
+        NOTE: DO NOT CALL THIS FILE, IT IS TO BE CALLED BY AN EMBEDDING INTERFACE.
 
-    @TODO: Check how well is this performing
 """
 
 import os
@@ -18,6 +18,7 @@ word2vec_embeddings = None
 glove_embeddings = None
 DEFAULT_EMBEDDING = 'word2vec'
 DEBUG = True
+PORT = 6969
 glove_location = \
     {
         'dir': "./resources",
@@ -26,9 +27,16 @@ glove_location = \
     }
 
 
-# Better warning formatting. Ignore.
-def better_warning(message, category, filename, lineno, file=None, line=None):
-    return ' %s:%s: %s:%s\n' % (filename, lineno, category.__name__, message)
+def start():
+    """
+        Call this function to
+            - start the server if not done already
+
+    :return: None
+    """
+    __check_prepared__(DEFAULT_EMBEDDING)
+    app = application = bottle.default_app()
+    bottle.run(server='gunicorn', host='127.0.0.1', port=PORT)
 
 
 def __check_prepared__(_embedding):
@@ -45,15 +53,6 @@ def __check_prepared__(_embedding):
             __prepare__(_word2vec=False, _glove=True)
 
 
-@post('/helloworld')
-def handshake():
-    """
-        Function to be called first when
-
-    :return:
-    """
-
-
 def __prepare__(_word2vec=True, _glove=False):
     """
         **Call this function prior to doing absolutely anything else.**
@@ -66,7 +65,8 @@ def __prepare__(_word2vec=True, _glove=False):
     if DEBUG: print("embeddings_interface: Loading Word Vector to Memory.")
 
     if _word2vec:
-        word2vec_embeddings = gensim.models.KeyedVectors.load_word2vec_format('resources/GoogleNews-vectors-negative300.bin', binary=True)
+        word2vec_embeddings = gensim.models.KeyedVectors.load_word2vec_format(
+            'resources/GoogleNews-vectors-negative300.bin', binary=True)
 
     if _glove:
         try:
@@ -74,7 +74,7 @@ def __prepare__(_word2vec=True, _glove=False):
         except IOError:
             # Glove is not parsed and stored. Do it.
             if DEBUG: warnings.warn(" GloVe is not parsed and stored. This will take some time.")
-    
+
             glove_embeddings = {}
             f = open(os.path.join(glove_location['dir'], glove_location['raw']))
             iterable = f
@@ -85,59 +85,42 @@ def __prepare__(_word2vec=True, _glove=False):
                 coefs = np.asarray(values[1:], dtype='float32')
                 glove_embeddings[word] = coefs
             f.close()
-    
+
             # Now convert this to a numpy object
             pickle.dump(glove_embeddings, open(os.path.join(glove_location['dir'], glove_location['parsed']), 'w+'))
-    
+
             if DEBUG: print("GloVe successfully parsed and stored. This won't happen again.")
 
 
 def __congregate__(_vector_set, ignore=[]):
     if len(ignore) == 0:
-        return np.mean(_vector_set, axis = 0)
+        return np.mean(_vector_set, axis=0)
     else:
         return np.dot(np.transpose(_vector_set), ignore) / sum(ignore)
 
 
-def phrase_similarity(_phrase_1, _phrase_2, embedding='word2vec'):
-
-    __check_prepared__(embedding)
-
-    phrase_1 = _phrase_1.split(" ")
-    phrase_2 = _phrase_2.split(" ")
-    vw_phrase_1 = []
-    vw_phrase_2 = []
-    for phrase in phrase_1:
-        try:
-            # print phrase
-            vw_phrase_2.append(word2vec_embeddings.word_vec(phrase.lower() if embedding == 'word2vec'
-                else glove_embeddings[phrase.lower()]))
-        except:
-            # print traceback.print_exc()
-            continue
-    for phrase in phrase_2:
-        try:
-            vw_phrase_2.append(word2vec_embeddings.word_vec(phrase.lower() if embedding == 'word2vec'
-                else glove_embeddings[phrase.lower()]))
-        except:
-            continue
-    if len(vw_phrase_1) == 0 or len(vw_phrase_2) == 0:
-        return 0
-    v_phrase_1 = __congregate__(vw_phrase_1)
-    v_phrase_2 = __congregate__(vw_phrase_2)
-    cosine_similarity = np.dot(v_phrase_1, v_phrase_2) / (np.linalg.norm(v_phrase_1) * np.linalg.norm(v_phrase_2))
-    return float(cosine_similarity)
-
-
-def __vectorize__(_tokens, _report_unks=False, _encode_special_chars=False, _embedding='glove'):
+# def __vectorize_(_tokens, _report_unks=False, _encode_special_chars=False, _embedding='glove'):
+def __vectorize__(data):
     """
         Function to embed a sentence and return it as a list of vectors.
         WARNING: Give it already split. I ain't splitting it for ye.
 
-        :param _tokens: The sentence you want embedded. (Assumed pre-tokenized input)
+        :param data: a dictionary containing some of the following:
+                    _tokens: List of Strings
+                    _encode_special_chars: Bool if special chars are to be encoded as well (for paths)
+                    __embedding: String-  either 'word2vec' or 'glove'
         :param _report_unks: Whether or not return the out of vocab words
         :return: Numpy tensor of n * 300d, [OPTIONAL] List(str) of tokens out of vocabulary.
     """
+    # Parse args from the dict
+    _tokens = data['_tokens']
+
+    try: _encode_special_chars = data['_encode_special_chars']
+    except KeyError: _encode_special_chars = False
+
+    try: _embedding = data['_embedding']
+    except KeyError: _embedding = DEFAULT_EMBEDDING
+    except ValueError: _embedding = DEFAULT_EMBEDDING
 
     __check_prepared__(_embedding)
 
@@ -155,7 +138,6 @@ def __vectorize__(_tokens, _report_unks=False, _encode_special_chars=False, _emb
                 token_embedding = word2vec_embeddings.word_vec(token)
 
         except KeyError:
-            if _report_unks: unks.append(token)
             token_embedding = np.zeros(300, dtype=np.float32)
 
         finally:
@@ -170,50 +152,61 @@ def __vectorize__(_tokens, _report_unks=False, _encode_special_chars=False, _emb
                     token_embedding = np.repeat(0.5, 300)
             op += [token_embedding]
 
-    return (np.asarray(op), unks) if _report_unks else np.asarray(op)
+    return np.asarray(op)
 
 
-"""
-    - check if embeddings are loaded or not.
-        - if not, load them.
-    - check if a bottle server is runnig
+@post('/setembedding')
+def set_embedding():
+    """
+        Call this URL with embedding of choice if you wanna ensure that the mentioned embedding matrix is in the RAM
 
-    - i
-"""
-@post('/names')
+    :return: None
+    """
+
+    print(request.json)
+    pass
+
+
+@get('/vectorize')
 def vectorize():
     """
-
-    :return:
+        Function to embed a sentence and return it as a list of vectors.
     """
+    try:
+        # Gather input data
+        try:
+            data = request.json
+        except:
+            raise ValueError
 
-    # try:
-    #     # parse input data
-    #     try:
-    #         data = request.json()
-    #     except:
-    #         raise ValueError
-    #
-    #     if data is None:
-    #         raise ValueError
-    #
-    # except ValueError:
-    #     # if bad request data, return 400 Bad Request
-    #     response.status = 400
-    #     return
-    #
-    # except KeyError:
-    #     # if name already exists, return 409 Conflict
-    #     response.status = 409
-    #     return
-    #
-    # # add nam
+        if data is None:
+            raise ValueError
 
-    # return 200 Success
+        # Check if the data is a dict, with all necessary things
+        if type(data) == dict and \
+                {'_embedding', '_encode_special_chars', '_report_unks', '_tokens'}.issuperset(set(data.keys())):
+            pass
+        else:
+            raise TypeError
+
+    except ValueError:
+        # No data at all, raise error.
+        response.status = 400
+        return
+
+    except TypeError:
+        # Invalid data. Raise Error.
+        response.status = 409
+        return
+
+    vectors = __vectorize__(data)
+
+    # Return 200 Success
     response.headers['Content-Type'] = 'application/json'
-    return json.dumps({'name': 'name'})
+    return json.dumps({'vectors': vectors,
+                       'request': data})
 
-app = application = bottle.default_app()
 
 if __name__ == '__main__':
-    bottle.run(server='gunicorn', host='127.0.0.1', port=8000)
+    os.chdir('..')
+    start()

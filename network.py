3556eccb96d626e89fe94261ffeb9c152ce69135
Priyansh Trivedi <pc.priyansh@gmail.com>
2018-07-27 10:29:45 +0200

    Fixed some syntax bugs and added another model: biLSTMDenseDot

--- network.py
+++ network.py
@@ -5,7 +5,7 @@
 import components as com
 import torch
 
-class BiLstmDot():
+class BiLstmDot:
 
     def __init__(self, _parameter_dict, _device, _pointwise=False, _debug=False):
         self.debug = _debug
@@ -96,21 +96,28 @@ class BiLstmDot():
         """
             Same code works for both pairwise or pointwise
         """
-
-        hidden = self.encoder.init_hidden(ques.shape[0], device)
-        question, _ = self.encoder(ques.long(), hidden)
-        paths, _ = self.encoder(paths.long(), hidden)
-        score = torch.sum(question[-1] * paths[-1], -1)
-        return score
+        with torch.no_grad():
+            hidden = self.encoder.init_hidden(ques.shape[0], device)
+            question, _ = self.encoder(ques.long(), hidden)
+            paths, _ = self.encoder(paths.long(), hidden)
+            score = torch.sum(question[-1] * paths[-1], -1)
+            return score
 
 
 class BiLstmDense:
+    """
+        This model replaces the dot product of BiLstmDot with a two layered dense classifier.
+        Plus, we only use the last state of the encoder in doing so.
+        Like before, we have both pairwise and pointwise versions.
+    """
 
     def __init__(self, _parameter_dict, _device, _pointwise=False, _debug=False):
         self.debug = _debug
         self.parameter_dict = _parameter_dict
         self.device = _device
         self.pointwise = _pointwise
+
+        self.hiddendim = self.parameter_dict['hidden_size'] * (2 * int(self.parameter_dict['bidirectional']))
 
         if self.debug:
             print("Init Models")
@@ -119,80 +126,222 @@ class BiLstmDense:
                                    self.parameter_dict['vocab_size'],
                                    bidirectional=self.parameter_dict['bidirectional'],
                                    vectors=self.parameter_dict['vectors']).cuda(self.device)
-        self.dense = com.DenseClf(inputdim=self.parameter_dict['hidden_size']*2,
-                                  hiddendim=self.parameter_dict['hidden_size'],
+        self.dense = com.DenseClf(inputdim=self.hiddendim,
+                                  hiddendim=self.hiddendim/2,
                                   outputdim=1)
 
-        def train(self, data, optimizer, loss_fn, device):
+    def train(self, data, optimizer, loss_fn, device):
 
-            if self.pointwise:
-                return self._train_pointwise_(data, optimizer, loss_fn, device)
-            else:
-                return self._train_pairwise_(data, optimizer, loss_fn, device)
+        if self.pointwise:
+            return self._train_pointwise_(data, optimizer, loss_fn, device)
+        else:
+            return self._train_pairwise_(data, optimizer, loss_fn, device)
 
-        def _train_pairwise_(self, data, optimizer, loss_fn, device):
-            '''
-                Given data, passes it through model, inited in constructor, returns loss and updates the weight
-                :params data: {batch of question, pos paths, neg paths and dummy y labels}
-                :params optimizer: torch.optim object
-                :params loss fn: torch.nn loss object
-                :params device: torch.device object
+    def _train_pairwise_(self, data, optimizer, loss_fn, device):
+        '''
+            Given data, passes it through model, inited in constructor, returns loss and updates the weight
+            :params data: {batch of question, pos paths, neg paths and dummy y labels}
+            :params optimizer: torch.optim object
+            :params loss fn: torch.nn loss object
+            :params device: torch.device object
 
-                returns loss
-            '''
+            returns loss
+        '''
 
-            # Unpacking the data and model from args
-            ques_batch, pos_batch, neg_batch, y_label = data['ques_batch'], data['pos_batch'], data['neg_batch'], data[
-                'y_label']
+        # Unpacking the data and model from args
+        ques_batch, pos_batch, neg_batch, y_label = data['ques_batch'], data['pos_batch'], data['neg_batch'], data[
+            'y_label']
 
-            hidden = self.encoder.init_hidden(ques_batch.shape[0], device)
-            optimizer.zero_grad()
-            # Encoding all the data
-            ques_batch, _ = self.encoder(ques_batch, hidden)
-            pos_batch, _ = self.encoder(pos_batch, hidden)
-            neg_batch, _ = self.encoder(neg_batch, hidden)
-            # Calculating dot score
-            pos_scores = self.dense(torch.cat((ques_batch[-1], pos_batch[-1]), dim=1))
-            neg_scores = self.dense(torch.cat((ques_batch[-1], neg_batch[-1]), dim=1))
-            #     torch.sum(ques_batch[-1] * pos_batch[-1], -1)
-            # neg_scores = torch.sum(ques_batch[-1] * neg_batch[-1], -1)
-            '''
-                If `y == 1` then it assumed the first input should be ranked higher
-                (have a larger value) than the second input, and vice-versa for `y == -1`
-            '''
-            loss = loss_fn(pos_scores, neg_scores, y_label)
-            loss.backward()
-            optimizer.step()
-            return loss
+        hidden = self.encoder.init_hidden(ques_batch.shape[0], device)
+        optimizer.zero_grad()
+        # Encoding all the data
+        ques_batch, _ = self.encoder(ques_batch, hidden)
+        pos_batch, _ = self.encoder(pos_batch, hidden)
+        neg_batch, _ = self.encoder(neg_batch, hidden)
+        # Calculating dot score
+        pos_scores = self.dense(torch.cat((ques_batch[-1], pos_batch[-1]), dim=1))
+        neg_scores = self.dense(torch.cat((ques_batch[-1], neg_batch[-1]), dim=1))
+        #     torch.sum(ques_batch[-1] * pos_batch[-1], -1)
+        # neg_scores = torch.sum(ques_batch[-1] * neg_batch[-1], -1)
+        '''
+            If `y == 1` then it assumed the first input should be ranked higher
+            (have a larger value) than the second input, and vice-versa for `y == -1`
+        '''
+        loss = loss_fn(pos_scores, neg_scores, y_label)
+        loss.backward()
+        optimizer.step()
+        return loss
 
-        def _train_pointwise_(self, data, optimizer, loss_fn, device):
-            '''
-                Given data, passes it through model, inited in constructor, returns loss and updates the weight
-                :params data: {batch of question, paths and y labels}
-                :params models list of [models]
-                :params optimizer: torch.optim object
-                :params loss fn: torch.nn loss object
-                :params device: torch.device object
+    def _train_pointwise_(self, data, optimizer, loss_fn, device):
+        '''
+            Given data, passes it through model, inited in constructor, returns loss and updates the weight
+            :params data: {batch of question, paths and y labels}
+            :params models list of [models]
+            :params optimizer: torch.optim object
+            :params loss fn: torch.nn loss object
+            :params device: torch.device object
 
-                returrns loss
-            '''
-            # Unpacking the data and model from args
-            ques_batch, path_batch, y_label = data['ques_batch'], data['path_batch'], data['y_label']
+            returrns loss
+        '''
+        # Unpacking the data and model from args
+        ques_batch, path_batch, y_label = data['ques_batch'], data['path_batch'], data['y_label']
 
-            hidden = self.encoder.init_hidden(ques_batch.shape[0], device)
-            optimizer.zero_grad()
+        hidden = self.encoder.init_hidden(ques_batch.shape[0], device)
+        optimizer.zero_grad()
 
-            # Encoding all the data
-            ques_batch, _ = self.encoder(ques_batch, hidden)
-            pos_batch, _ = self.encoder(path_batch, hidden)
+        # Encoding all the data
+        ques_batch, _ = self.encoder(ques_batch, hidden)
+        pos_batch, _ = self.encoder(path_batch, hidden)
 
-            # Calculating dot score
-            score = self.dense(torch.cat((ques_batch[-1], pos_batch[-1]), dim=1))
+        # Calculating dot score
+        score = self.dense(torch.cat((ques_batch[-1], pos_batch[-1]), dim=1))
 
-            '''
-                Binary Cross Entropy loss function. @TODO: Check if we can give it 1/0 labels.
-            '''
-            loss = loss_fn(score, y_label)
-            loss.backward()
-            optimizer.step()
-            return loss
+        '''
+            Binary Cross Entropy loss function. @TODO: Check if we can give it 1/0 labels.
+        '''
+        loss = loss_fn(score, y_label)
+        loss.backward()
+        optimizer.step()
+        return loss
+
+    def predict(self, ques, paths, device):
+        """
+            Same code works for both pairwise or pointwise
+        """
+
+        with torch.no_grad():
+
+            hidden = self.encoder.init_hidden(ques.shape[0], device)
+            question, _ = self.encoder(ques.long(), hidden)
+            paths, _ = self.encoder(paths.long(), hidden)
+            score = self.dense(torch.cat((question[-1], paths[-1]), dim=1))
+            return score
+
+
+class BiLstmDenseDot:
+    """
+        This model uses the encoder, then condenses the vector into something of a smaller dimension.
+        Then uses a regular dot product to compute the final deal.
+        Plus, we only use the last state of the encoder in doing so.
+        Like before, we have both pairwise and pointwise versions.
+    """
+
+    def __init__(self, _parameter_dict, _device, _pointwise=False, _debug=False):
+        self.debug = _debug
+        self.parameter_dict = _parameter_dict
+        self.device = _device
+        self.pointwise = _pointwise
+
+        self.hiddendim = self.parameter_dict['hidden_size'] * (2 * int(self.parameter_dict['bidirectional']))
+
+        if self.debug:
+            print("Init Models")
+        self.encoder = com.Encoder(self.parameter_dict['max_length'], self.parameter_dict['hidden_size'],
+                                   self.parameter_dict['number_of_layer'], self.parameter_dict['embedding_dim'],
+                                   self.parameter_dict['vocab_size'],
+                                   bidirectional=self.parameter_dict['bidirectional'],
+                                   vectors=self.parameter_dict['vectors']).cuda(self.device)
+        self.dense = com.DenseClf(inputdim=self.hiddendim,
+                                  hiddendim=self.hiddendim/2,
+                                  outputdim=self.hiddendim/4)
+
+    def train(self, data, optimizer, loss_fn, device):
+
+        if self.pointwise:
+            return self._train_pointwise_(data, optimizer, loss_fn, device)
+        else:
+            return self._train_pairwise_(data, optimizer, loss_fn, device)
+
+    def _train_pairwise_(self, data, optimizer, loss_fn, device):
+        '''
+            Given data, passes it through model, inited in constructor, returns loss and updates the weight
+            :params data: {batch of question, pos paths, neg paths and dummy y labels}
+            :params optimizer: torch.optim object
+            :params loss fn: torch.nn loss object
+            :params device: torch.device object
+
+            returns loss
+        '''
+
+        # Unpacking the data and model from args
+        ques_batch, pos_batch, neg_batch, y_label = data['ques_batch'], data['pos_batch'], data['neg_batch'], data[
+            'y_label']
+
+        hidden = self.encoder.init_hidden(ques_batch.shape[0], device)
+        optimizer.zero_grad()
+        # Encoding all the data
+        ques_batch, _ = self.encoder(ques_batch, hidden)
+        pos_batch, _ = self.encoder(pos_batch, hidden)
+        neg_batch, _ = self.encoder(neg_batch, hidden)
+
+        # Pass all encoded stuff through the dense too
+        ques_batch = self.dense(ques_batch[-1])
+        pos_batch = self.dense(pos_batch[-1])
+        neg_batch = self.dense(neg_batch[-1])
+
+        # Calculating dot score
+        pos_scores = torch.sum(ques_batch * pos_batch, -1)
+        neg_scores = torch.sum(ques_batch * neg_batch, -1)
+        '''
+            If `y == 1` then it assumed the first input should be ranked higher
+            (have a larger value) than the second input, and vice-versa for `y == -1`
+        '''
+        loss = loss_fn(pos_scores, neg_scores, y_label)
+        loss.backward()
+        optimizer.step()
+        return loss
+
+    def _train_pointwise_(self, data, optimizer, loss_fn, device):
+        '''
+            Given data, passes it through model, inited in constructor, returns loss and updates the weight
+            :params data: {batch of question, paths and y labels}
+            :params models list of [models]
+            :params optimizer: torch.optim object
+            :params loss fn: torch.nn loss object
+            :params device: torch.device object
+
+            returrns loss
+        '''
+        # Unpacking the data and model from args
+        ques_batch, path_batch, y_label = data['ques_batch'], data['path_batch'], data['y_label']
+
+        hidden = self.encoder.init_hidden(ques_batch.shape[0], device)
+        optimizer.zero_grad()
+
+        # Encoding all the data
+        ques_batch, _ = self.encoder(ques_batch, hidden)
+        pos_batch, _ = self.encoder(path_batch, hidden)
+
+        # Compress the last state
+        ques_batch = self.dense(ques_batch[-1])
+        pos_batch = self.dense(pos_batch[-1])
+
+        # Calculating dot score
+        score = score = torch.sum(ques_batch[-1] * pos_batch[-1], -1)
+
+        '''
+            Binary Cross Entropy loss function. @TODO: Check if we can give it 1/0 labels.
+        '''
+        loss = loss_fn(score, y_label)
+        loss.backward()
+        optimizer.step()
+
+        return loss
+
+    def predict(self, ques, paths, device):
+        """
+            Same code works for both pairwise or pointwise
+        """
+        with torch.no_grad():
+
+            hidden = self.encoder.init_hidden(ques.shape[0], device)
+            question, _ = self.encoder(ques.long(), hidden)
+            paths, _ = self.encoder(paths.long(), hidden)
+
+            # Dense
+            question = self.dense(question[-1])
+            paths = self.dense(paths[-1])
+
+            score = torch.sum(question * paths, -1)
+
+            return score
